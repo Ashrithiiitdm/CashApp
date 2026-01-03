60275d7c1ed7c88eaa1046386c7a6cf204cc4234
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2026-01-03 16:28:55 +0530

    Added Add money

--- migrations/002_fix_wallet_transactions_currency.sql
+++
@@ -1,7 +0,0 @@
--- Fix currency column to support 3-character currency codes like 'INR'
-ALTER TABLE public.wallet_transactions 
-ALTER COLUMN currency TYPE character(3);
-
--- Update the default value properly
-ALTER TABLE public.wallet_transactions 
-ALTER COLUMN currency SET DEFAULT 'INR'::bpchar;
